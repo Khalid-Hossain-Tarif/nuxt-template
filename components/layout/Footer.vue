db6be6448553ce9cdd9d6830403d43ab52294c2e
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2025-01-09 14:23:46 +0600

    update: header and footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -2,13 +2,20 @@
 </script>
 
 <template>
-  <header>
-    <div class="container">
-      <div class="flex items-center justify-between gap-x-4">
-        <div>
-          Nuxt Template
-        </div>
+  <footer class="bg-background-shade3 border-b border-borderColor-secondary">
+    <div class="container py-20">
+      <div class="space-y-2.5 text-center">
+        <nuxt-link to="/" class="text-primary hover:text-secondary font-semibold text-lg">
+          Nuxt
+        </nuxt-link>
+        <nav>
+          <ul class="flex items-center justify-center gap-x-5">
+            <li><NuxtLink to="/" class="text-dark-secondary hover:text-secondary">Home</NuxtLink></li>
+            <li><NuxtLink to="/about" class="text-dark-secondary hover:text-secondary">About</NuxtLink></li>
+            <li><NuxtLink to="/contact" class="text-dark-secondary hover:text-secondary">Contact</NuxtLink></li>
+          </ul>
+        </nav>
       </div>
     </div>
-  </header>
+  </footer>
 </template>
--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -5,18 +5,24 @@
   <header class="bg-background-shade3 border-b border-borderColor-secondary">
     <div class="container py-5">
       <div class="flex items-center justify-between gap-x-4">
-        <a class="text-secondary font-semibold text-lg">
+        <nuxt-link to="/" class="shrink-0 text-secondary hover:text-primary font-semibold text-lg">
           Nuxt Template
-        </a>
+        </nuxt-link>
 
         <nav>
-          <ul>
-            <li><NuxtLink to="/about">About</NuxtLink></li>
-            <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
-            <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+          <ul class="flex items-center gap-x-5">
+            <li><NuxtLink to="/" class="text-dark hover:text-secondary">Home</NuxtLink></li>
+            <li><NuxtLink to="/about" class="text-dark hover:text-secondary">About</NuxtLink></li>
+            <li><NuxtLink to="/contact" class="text-dark hover:text-secondary">Contact</NuxtLink></li>
           </ul>
         </nav>
       </div>
     </div>
   </header>
 </template>
+
+<style scoped>
+.router-link-exact-active {
+  @apply font-medium text-secondary
+}
+</style>
