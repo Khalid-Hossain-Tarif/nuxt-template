de8655faf77ccaa915698e882956cbaa3478f529
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2025-01-28 15:43:08 +0600

    update(icones): responsive design - icones icon component

--- components/icons/Bell.vue
+++ components/icons/Bell.vue
@@ -1,9 +1,8 @@
-<script>
-export default {
-  name: 'MaterialSymbolsAddAlert'
-}
+<script setup>
+import { cn } from '@/lib/utils'
+const props = defineProps(['class'])
 </script>
 
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 19v-2h2v-7q0-2.075 1.25-3.687T10.5 4.2v-.7q0-.625.438-1.062T12 2t1.063.438T13.5 3.5v.7q2 .5 3.25 2.113T18 10v7h2v2zm8 3q-.825 0-1.412-.587T10 20h4q0 .825-.587 1.413T12 22m-1-7h2v-2h2v-2h-2V9h-2v2H9v2h2z"></path></svg>
+  <svg :class="cn('w-4 lg:w-6', props.class)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M4 19v-2h2v-7q0-2.075 1.25-3.687T10.5 4.2v-.7q0-.625.438-1.062T12 2t1.063.438T13.5 3.5v.7q2 .5 3.25 2.113T18 10v7h2v2zm8 3q-.825 0-1.412-.587T10 20h4q0 .825-.587 1.413T12 22m-1-7h2v-2h2v-2h-2V9h-2v2H9v2h2z"></path></svg>
 </template>
