fe13e2d96d4b89e4f3c230414178c19f37ab9654
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2025-02-03 13:09:13 +0600

    update: product card

--- components/products/ProductList.vue
+++ components/products/ProductList.vue
@@ -1,29 +1,40 @@
 <script setup lang="ts">
   import { displayPrice } from "~/utils/Helper";
+  import IconsTag from "~/components/icons/Tag.vue";
 
-  const props = defineProps({
-    products: Array<object>,
-  });
+  const props = defineProps<{
+    products: {
+      type: Array<any>;
+      required: true;
+      default: () => [];
+    };
+  }>();
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-4">
     <div
       v-for="product in products"
-      :key="product.id"
-      class="border border-gray-200 rounded-lg"
+      :key="product?.id"
+      class="relative border border-gray-200 rounded-lg"
     >
       <img
-        :src="product.images[0]"
-        :alt="product.title"
+        :src="product?.images[0]"
+        :alt="product?.title"
         class="rounded-t-lg"
       />
       <div class="p-4">
         <h4 class="text-dark text-xl font-medium line-clamp-1 capitalize">
-          {{ product.title }}
+          {{ product?.title }}
         </h4>
-        <p class="mt-1 text-dark-secondary">
-          {{ displayPrice(product.price) }}
+        <p class="my-1 text-dark-secondary">
+          {{ displayPrice(product?.price) }}
+        </p>
+        <p
+          class="flex items-center gap-x-1 py-1 px-2 rounded bg-secondary-light text-secondary text-xs font-medium absolute top-3 left-3"
+        >
+          <IconsTag class="lg:w-4" />
+          <span>{{ product?.category?.name }}</span>
         </p>
       </div>
     </div>
